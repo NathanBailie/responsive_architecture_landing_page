22163a20f60db386d4af450457eaa07e8298d832
Nathan <antnefedov33@gmail.com>
2024-10-22 16:04:01 +0300

    main section, added swiper with parallax effect, fixes

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,43 @@
 'use strict';
+import Swiper from '../libs/swiper-bundle.min.js';
 
 const navbar_toggler = document.querySelector('.navbar_toggler');
 const navbar_menu = document.querySelector('.navbar_menu');
-const navbar_menu_links = document.querySelectorAll('.navbar_menu li');
+const navbar_menu_links = document.querySelectorAll('.navbar_menu li a');
 
 navbar_toggler.addEventListener('click', () => {
 	navbar_toggler.classList.toggle('active');
 	navbar_menu.classList.toggle('mobile');
 });
 
-navbar_menu_links.forEach(li => {
-	li.addEventListener('click', () => {
+navbar_menu_links.forEach(link => {
+	link.addEventListener('click', () => {
 		navbar_toggler.classList.toggle('active');
 		navbar_menu.classList.toggle('mobile');
 	});
+});
+
+// swiper
+const swiperHome = new Swiper('.home_swiper', {
+	loop: true,
+	speed: 800,
+	parallax: true,
+	effect: 'fade',
+
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'fraction',
+
+		formatFractionCurrent: n => {
+			return '0' + n;
+		},
+		formatFractionTotal: n => {
+			return '0' + n;
+		},
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
